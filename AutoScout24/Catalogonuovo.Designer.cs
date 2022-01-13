23dcce843fb22bbad90d69dff42390dc5b31eebc
RaphacePrime <raphael.gemello@gmail.com>
2022-01-13 11:44:48 +0100

    Modifica Catalogo

---
+++ AutoScout24/Catalogonuovo.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace AutoScout24
+{
+    partial class Catalogonuovo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carrellolista = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.cnmarca = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carrellolista
+            // 
+            this.carrellolista.FormattingEnabled = true;
+            this.carrellolista.Location = new System.Drawing.Point(1060, 135);
+            this.carrellolista.Name = "carrellolista";
+            this.carrellolista.Size = new System.Drawing.Size(357, 154);
+            this.carrellolista.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1114, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 77);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CARRELLO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(424, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(377, 115);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CATALOGO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-2, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Marca:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Yi Baiti", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-2, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Modello: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Yi Baiti", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(-2, 347);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Username:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Yi Baiti", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(485, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 33);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Costo:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(352, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(510, 57);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Aggiungi al carrello";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::AutoScout24.Properties.Resources.Back;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(269, 414);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(68, 59);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(296, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(628, 251);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::AutoScout24.Properties.Resources.Back___Copia;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(880, 414);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(68, 59);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // cnmarca
+            // 
+            this.cnmarca.AutoSize = true;
+            this.cnmarca.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20F);
+            this.cnmarca.Location = new System.Drawing.Point(136, 190);
+            this.cnmarca.Name = "cnmarca";
+            this.cnmarca.Size = new System.Drawing.Size(66, 27);
+            this.cnmarca.TabIndex = 11;
+            this.cnmarca.Text = ".........";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20F);
+            this.label8.Location = new System.Drawing.Point(136, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 27);
+            this.label8.TabIndex = 12;
+            this.label8.Text = ".........";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20F);
+            this.label9.Location = new System.Drawing.Point(136, 347);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 27);
+            this.label9.TabIndex = 13;
+            this.label9.Text = ".........";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20F);
+            this.label10.Location = new System.Drawing.Point(577, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 27);
+            this.label10.TabIndex = 14;
+            this.label10.Text = ".........";
+            // 
+            // Catalogonuovo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(1444, 606);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cnmarca);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.carrellolista);
+            this.Name = "Catalogonuovo";
+            this.Text = "Catalogonuovo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox carrellolista;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label cnmarca;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
--- AutoScout24/Properties/Resources.Designer.cs
+++ AutoScout24/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace AutoScout24.Properties {
         /// <summary>
         ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Back___Copia {
+            get {
+                object obj = ResourceManager.GetObject("Back - Copia", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap carrello {
             get {
                 object obj = ResourceManager.GetObject("carrello", resourceCulture);
