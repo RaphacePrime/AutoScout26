4928186a144cb46597a9fa7c524e2183639f2ad3
Luzzi03 <luzzananicola7a7gmail.com>
2022-01-18 12:21:58 +0100

    Creazione form catalogo

---
+++ AutoScout24/Catalogo2.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace AutoScout24
+{
+    partial class Catalogo2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ListaVeicoli = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(283, 158);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(119, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // ListaVeicoli
+            // 
+            this.ListaVeicoli.FormattingEnabled = true;
+            this.ListaVeicoli.Location = new System.Drawing.Point(49, 158);
+            this.ListaVeicoli.Name = "ListaVeicoli";
+            this.ListaVeicoli.Size = new System.Drawing.Size(120, 95);
+            this.ListaVeicoli.TabIndex = 2;
+            this.ListaVeicoli.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::AutoScout24.Properties.Resources.carrello;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(309, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 63);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::AutoScout24.Properties.Resources.Aggiungi;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(69, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 63);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 57);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Catalogo";
+            // 
+            // Catalogo2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(464, 452);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ListaVeicoli);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Catalogo2";
+            this.Text = "Catalogo2";
+            this.Load += new System.EventHandler(this.Catalogo2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox ListaVeicoli;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AutoScout24/Catalogo2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoScout24
+{
+    public partial class Catalogo2 : Form
+    {
+        System.Data.OleDb.OleDbConnection conn;
+        public Catalogo2()
+        {
+            InitializeComponent();
+        }
+
+        private void Catalogo2_Load(object sender, EventArgs e)
+        {
+            string connstr;
+            connstr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=MazzoLuzzi.accdb;";
+            conn = new System.Data.OleDb.OleDbConnection(connstr);
+            conn.Open();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Data.OleDb.OleDbCommand cmd;
+            System.Data.OleDb.OleDbDataReader rs;
+
+
+            cmd = new System.Data.OleDb.OleDbCommand();
+            cmd.Connection = conn;
+
+            string sql;
+            sql = "SELECT Classe, Nome FROM Prodotti;";
+            cmd.CommandText = sql;
+
+            rs = cmd.ExecuteReader();
+            ListaVeicoli.Text = "";
+            while (rs.Read())
+            {
+                ListaVeicoli.
+            }
+        }
+    }
+}
